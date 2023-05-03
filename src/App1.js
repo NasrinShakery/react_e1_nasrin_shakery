d87004e98eb2e7ad709197aa7a94834f9e286d35
nansiiii <nasrin.shakery8@gmail.com>
2023-05-03 10:15:45 +0330

    start

---
+++ src/App1.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App1 extends Component {
+    state = {  } 
+    render() { 
+        return (
+            <>
+                <App1></App1>
+            </>
+        );
+    }
+}
+ 
+export default App1;
